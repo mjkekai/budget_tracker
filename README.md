711f62acb1477ce8fa24819b0f53305d1165088a
Matt Koski <mjkekai@gmail.com>
2021-08-27 14:10:03 -0700

    adds readme

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# budget_tracker
+# budget tracker
+
+![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+## Table of Contents (Optional)
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Description](#description)
+- [License](#license)
+- [Credits](#credits)
+- [Contributing](#contributing)
+
+## Description
+
+an online and offline budget tracker
+
+## Usage
+
+As a user i want to be able to enter in transactions to either add funds that have been deposited to my account or remove funds that i have spent. When entered offline those transactions will update my account when i am back online.
+
+## Contributing
+
+none
+
+## Installation
+
+will need to add dependencies with npm i and use node serve.js to run the application on local host 3000
+
+## Credits
+
+none
+
+## License
+
+MIT
---
+++ public/serviceworker.js
@@ -0,0 +1,52 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/db.js',
+    '/index.html',
+    '/style.css',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'    
+];
+  
+  const PRECACHE = 'precache-v1';
+
+  
+  self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches
+        .open(PRECACHE)
+        .then((cache) => cache.addAll(FILES_TO_CACHE))
+        .then(self.skipWaiting())
+    );
+  });
+  
+  self.addEventListener("fetch", function(event) {
+    if (event.request.url.includes("/api/")) {
+      event.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(event.request)
+            .then(response => {
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+              return response;
+            })
+            .catch(err => {
+              return cache.match(event.request);
+            });
+        }).catch(err => console.log(err))
+      );
+      return;
+    }
+    event.respondWith(
+      fetch(event.request).catch(function() {
+        return caches.match(event.request).then(function(response) {
+          if (response) {
+            return response;
+          } else if (event.request.headers.get("accept").includes("text/html")) {
+            return caches.match("/");
+          }
+        });
+      })
+    );
+  });
+  
